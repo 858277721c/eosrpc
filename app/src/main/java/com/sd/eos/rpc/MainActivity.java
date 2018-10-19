2cb975e5ce1addac383cd6464b71e88ad125f2f1
zjun <565061763@qq.com>
2018-10-20 01:27:04 +0800

    refactor

--- app/src/main/java/com/sd/eos/rpc/MainActivity.java
+++ app/src/main/java/com/sd/eos/rpc/MainActivity.java
@@ -12,6 +12,11 @@ import com.sd.lib.http.task.FTask;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final String CREATER_ACCOUNT = "fwtest111111";
+    public static final String CREATER_KEY_PRIVATE = "5JQh92zjfWJW6a6suaLm7o2wMCNKZnqp4bb3Kb2dNSb7ts8vDWH";
+    public static final String CREATER_KEY_PUBLIC = "EOS67Q8sXouXtxi72FFgih5hWx7Ks1Bkm8soNuzt9U9sRNuMtjHwT";
+
+
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -20,26 +25,31 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createAccount("lanfang12345", "EOS6tz2NBXZDQLxt5a7tiWVvfuqDgNuVDJRReKZBn5xCWroagzZa9");
+    }
+
+    private void createAccount(final String name, final String publicKey)
+    {
         new FTask()
         {
             @Override
             protected void onRun() throws Exception
             {
                 final NewaccountActionParams newaccountActionParams = new NewaccountActionParams.Builder()
-                        .setCreator("fwtest111111")
-                        .setName("lanfang12345")
-                        .setOwner("EOS6tz2NBXZDQLxt5a7tiWVvfuqDgNuVDJRReKZBn5xCWroagzZa9")
+                        .setCreator(CREATER_ACCOUNT)
+                        .setName(name)
+                        .setOwner(publicKey)
                         .build();
 
                 final BuyramActionParams buyramActionParams = new BuyramActionParams.Builder()
-                        .setPayer("fwtest111111")
-                        .setReceiver("lanfang12345")
+                        .setPayer(CREATER_ACCOUNT)
+                        .setReceiver(name)
                         .setQuant("1.0000 EOS")
                         .build();
 
                 final DelegatebwActionParams delegatebwActionParams = new DelegatebwActionParams.Builder()
-                        .setFrom("fwtest111111")
-                        .setReceiver("lanfang12345")
+                        .setFrom(CREATER_ACCOUNT)
+                        .setReceiver(name)
                         .setStake_cpu_quantity("1.0000 EOS")
                         .setStake_net_quantity("1.0000 EOS")
                         .setTransfer(1)
@@ -49,7 +59,7 @@ public class MainActivity extends AppCompatActivity
                 pushManager.addAction(newaccountActionParams);
                 pushManager.addAction(buyramActionParams);
                 pushManager.addAction(delegatebwActionParams);
-                pushManager.execute("5JQh92zjfWJW6a6suaLm7o2wMCNKZnqp4bb3Kb2dNSb7ts8vDWH");
+                pushManager.execute(CREATER_KEY_PRIVATE);
             }
 
             @Override

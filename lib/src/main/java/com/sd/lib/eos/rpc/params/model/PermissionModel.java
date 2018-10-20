129b2c9c07c016bdc504dfec3ab55885be4cf6d3
zjun <565061763@qq.com>
2018-10-20 13:56:07 +0800

    refactor

--- lib/src/main/java/com/sd/lib/eos/rpc/params/model/PermissionModel.java
+++ lib/src/main/java/com/sd/lib/eos/rpc/params/model/PermissionModel.java
@@ -8,15 +8,21 @@ public class PermissionModel
     private final int threshold;
     private final List<KeyModel> keys;
 
+    private final String[] accounts;
+    private final String[] waits;
+
     public PermissionModel(int threshold, KeyModel... keys)
     {
         this.threshold = threshold;
         this.keys = Arrays.asList(keys);
+
+        this.accounts = new String[]{};
+        this.waits = new String[]{};
     }
 
     public static PermissionModel create(String publicKey)
     {
-       return new PermissionModel(1, new KeyModel(publicKey, 1));
+        return new PermissionModel(1, new KeyModel(publicKey, 1));
     }
 
     public int getThreshold()

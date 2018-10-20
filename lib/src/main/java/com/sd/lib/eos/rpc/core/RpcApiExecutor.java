a325f0b8c60f5690a548d4f82d07934404720571
zjun <565061763@qq.com>
2018-10-20 22:45:56 +0800

    refactor

--- lib/src/main/java/com/sd/lib/eos/rpc/core/RpcApiExecutor.java
+++ lib/src/main/java/com/sd/lib/eos/rpc/core/RpcApiExecutor.java
@@ -2,7 +2,21 @@ package com.sd.lib.eos.rpc.core;
 
 import java.util.Map;
 
+/**
+ * RPC接口执行器
+ */
 public interface RpcApiExecutor
 {
+    /**
+     * 请求接口
+     *
+     * @param baseUrl 接口地址
+     * @param path    接口路径
+     * @param params  参数
+     * @param clazz   返回接口类
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
     <T> T execute(String baseUrl, String path, Map<String, Object> params, Class<T> clazz) throws Exception;
 }
--- lib/src/main/java/com/sd/lib/eos/rpc/core/TransactionSigner.java
+++ lib/src/main/java/com/sd/lib/eos/rpc/core/TransactionSigner.java
@@ -5,7 +5,19 @@ import com.sd.lib.eos.rpc.api.model.GetInfoResponse;
 import com.sd.lib.eos.rpc.output.model.TransactionQuery;
 import com.sd.lib.eos.rpc.output.model.TransactionSignResult;
 
+/**
+ * 交易签名接口
+ */
 public interface TransactionSigner
 {
+    /**
+     * 签名
+     *
+     * @param query
+     * @param infoResponse
+     * @param blockResponse
+     * @param privateKey
+     * @return
+     */
     TransactionSignResult signTransaction(TransactionQuery query, GetInfoResponse infoResponse, GetBlockResponse blockResponse, String privateKey);
 }

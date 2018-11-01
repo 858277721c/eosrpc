cec0d2c1ef28774888d4a92e9325c9b80264f75c
zjun <565061763@qq.com>
2018-11-01 15:07:09 +0800

    GetAccountResponse

--- lib/src/main/java/com/sd/lib/eos/rpc/api/model/GetAccountResponse.java
+++ lib/src/main/java/com/sd/lib/eos/rpc/api/model/GetAccountResponse.java
@@ -430,7 +430,50 @@ public class GetAccountResponse
 
     public static class RefundRequest
     {
+        private String owner;
+        private String request_time;
+        private String net_amount;
+        private String cpu_amount;
 
+        public String getOwner()
+        {
+            return owner;
+        }
+
+        public void setOwner(String owner)
+        {
+            this.owner = owner;
+        }
+
+        public String getRequest_time()
+        {
+            return request_time;
+        }
+
+        public void setRequest_time(String request_time)
+        {
+            this.request_time = request_time;
+        }
+
+        public String getNet_amount()
+        {
+            return net_amount;
+        }
+
+        public void setNet_amount(String net_amount)
+        {
+            this.net_amount = net_amount;
+        }
+
+        public String getCpu_amount()
+        {
+            return cpu_amount;
+        }
+
+        public void setCpu_amount(String cpu_amount)
+        {
+            this.cpu_amount = cpu_amount;
+        }
     }
 
     public static class VoterInfo

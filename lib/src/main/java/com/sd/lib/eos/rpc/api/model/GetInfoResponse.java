28b59f19ce1ac3a973f0de6709e59dcb25659021
zjun <565061763@qq.com>
2018-11-01 15:23:02 +0800

    GetInfoResponse

--- lib/src/main/java/com/sd/lib/eos/rpc/api/model/GetInfoResponse.java
+++ lib/src/main/java/com/sd/lib/eos/rpc/api/model/GetInfoResponse.java
@@ -6,9 +6,15 @@ public class GetInfoResponse
     private String chain_id;
     private long head_block_num;
     private long last_irreversible_block_num;
+    private String last_irreversible_block_id;
     private String head_block_id;
     private String head_block_time;
     private String head_block_producer;
+    private long virtual_block_cpu_limit;
+    private long virtual_block_net_limit;
+    private long block_cpu_limit;
+    private long block_net_limit;
+    private String server_version_string;
 
     public String getServer_version()
     {
@@ -50,6 +56,16 @@ public class GetInfoResponse
         this.last_irreversible_block_num = last_irreversible_block_num;
     }
 
+    public String getLast_irreversible_block_id()
+    {
+        return last_irreversible_block_id;
+    }
+
+    public void setLast_irreversible_block_id(String last_irreversible_block_id)
+    {
+        this.last_irreversible_block_id = last_irreversible_block_id;
+    }
+
     public String getHead_block_id()
     {
         return head_block_id;
@@ -78,5 +94,55 @@ public class GetInfoResponse
     public void setHead_block_producer(String head_block_producer)
     {
         this.head_block_producer = head_block_producer;
+    }
+
+    public long getVirtual_block_cpu_limit()
+    {
+        return virtual_block_cpu_limit;
+    }
+
+    public void setVirtual_block_cpu_limit(long virtual_block_cpu_limit)
+    {
+        this.virtual_block_cpu_limit = virtual_block_cpu_limit;
+    }
+
+    public long getVirtual_block_net_limit()
+    {
+        return virtual_block_net_limit;
+    }
+
+    public void setVirtual_block_net_limit(long virtual_block_net_limit)
+    {
+        this.virtual_block_net_limit = virtual_block_net_limit;
+    }
+
+    public long getBlock_cpu_limit()
+    {
+        return block_cpu_limit;
+    }
+
+    public void setBlock_cpu_limit(long block_cpu_limit)
+    {
+        this.block_cpu_limit = block_cpu_limit;
+    }
+
+    public long getBlock_net_limit()
+    {
+        return block_net_limit;
+    }
+
+    public void setBlock_net_limit(long block_net_limit)
+    {
+        this.block_net_limit = block_net_limit;
+    }
+
+    public String getServer_version_string()
+    {
+        return server_version_string;
+    }
+
+    public void setServer_version_string(String server_version_string)
+    {
+        this.server_version_string = server_version_string;
     }
 }

a5a9b1db1744369625d2df437a4ec8eedf545621
zhengjun <565061763@qq.com>
2018-11-28 21:25:13 +0800

    ErrorResponse

--- lib/src/main/java/com/sd/lib/eos/rpc/api/model/ErrorResponse.java
+++ lib/src/main/java/com/sd/lib/eos/rpc/api/model/ErrorResponse.java
@@ -8,25 +8,35 @@ public class ErrorResponse
     private String message;
     private Error error;
 
-    public String getErrorInformation()
+    public int getErrorCode()
+    {
+        return error == null ? 0 : error.getCode();
+    }
+
+    public String getErrorMessage()
+    {
+        if (error == null)
+            return "";
+
+        final List<Error.Details> details = error.getDetails();
+        if (details == null || details.isEmpty())
+            return error.getName() + " " + error.getWhat();
+
+        return details.get(0).getMessage();
+    }
+
+    public String getFormattedMessage()
     {
         final StringBuilder sb = new StringBuilder();
-
         if (error != null)
         {
-            sb.append("code:").append(error.getCode()).append("\n");
-            sb.append("details:");
-
-            final List<Error.Details> details = error.getDetails();
-            if (details != null && !details.isEmpty())
-            {
-                sb.append(details.get(0).message);
-            }
+            sb.append("code:").append(getErrorCode()).append("\n");
+            sb.append("details:").append(getErrorMessage());
         } else
         {
-            sb.append(code).append(" ").append(message).append("\n");
+            sb.append("code:").append(code).append("\n");
+            sb.append("details:").append(message);
         }
-
         return sb.toString();
     }
 
